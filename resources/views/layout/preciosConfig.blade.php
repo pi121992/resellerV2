407100b17c5938b25eb178d5f0fedb663d89d0ea
Miguel Z <pituntizelaya@gmail.com>
2020-03-08 04:09:45 -0500

    se creo un componente para los precios

---
+++ resources/views/layout/preciosConfig.blade.php
@@ -0,0 +1,51 @@
+<div class="row">
+
+     <div class="col-12 col-md-4">
+       @component('components.price',[
+        'type'=>'warning',
+        'btn'=>'Incia tu demo',
+        'title'=>'Demo',
+        'price'=>'$0',
+        'url'=>'/demo'
+        ])
+       @endcomponent
+     </div>
+
+     <div class="col-12 col-md-4">
+       @component('components.price',[
+        'title'=>'Basic / $5 each',
+        'price'=>'$5',
+        'btn'=>"Inicia con 1 credito"
+
+        ])
+         
+       @endcomponent
+     </div>
+     <div class="col-12 col-md-4">
+       @component('components.price',[
+        'title'=>'Premiun/ $4 each',
+        'price'=>'$20',
+        'btn'=>"Inicia con 5 creditos"]
+        )
+        
+       
+       @endcomponent
+       </div>
+       <div class="col-12 col-md-4">
+       @component('components.price',[
+        'title'=>'Advanced/ $3.5 each',
+        'price'=>'$35',
+        'btn'=>"Inicia con 10 creditos"
+        ])
+        @endcomponent
+     </div>
+     <div class="col-12 col-md-4">
+       @component('components.price',[
+        'title'=>'Master/ $3 each',
+        'price'=>'$60',
+        'btn'=>"Inicia con 20 creditos"])
+       
+     @endcomponent
+     </div>
+
+   </div>
