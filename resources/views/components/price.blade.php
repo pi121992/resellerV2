4842b57cbab7ac9b0c5c2bf8856901f3846d0dba
Miguel Z <pituntizelaya@gmail.com>
2020-03-08 03:50:12 -0500

    actulizacion de los precios

--- resources/views/components/price.blade.php
+++ resources/views/components/price.blade.php
@@ -7,6 +7,11 @@
             <h1 class="card-title pricing-card-title">{{ $price }} <small class="text-muted"></small></h1>
             <ul class="list-unstyled mt-3 mb-4">
               {{ $slot }}
+             <li>Cada credito tiene duracion de 1 mes</li>
+             <li>Tu tienes el control de tu cuenta</li>
+             <li>2 conecciones simultaneas</li>
+             <li>Mas de 40TB de contenido</li>
+             <li>Actualizaciones diarias</li>
             </ul>
           
            <a class="btn btn-lg btn-block btn-{{ $type ?? 'info' }}" href="{{ $url ?? 'https://t.me/compraplex' }}">{{ $btn ?? 'Inicia hoy' }}</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
    <div class="row">
+
      <div class="col-12 col-md-4">
        @component('components.price',[
         'type'=>'warning',
@@ -10,47 +11,40 @@
         'price'=>'$0',
         'url'=>'/demo'
         ])
-       <li>Demo solicita tu Demo has  <a href="/demo">click aqui</a> </li>
-         <li>Tu tienes el control de tu cuenta</li>
-         <li>2 conecciones simultaneas</li>
-         <li>Mas de 40TB de contenido</li>
-         <li>Actualizaciones diarias</li>
+       @endcomponent
+     </div>
+
+     <div class="col-12 col-md-4">
+       @component('components.price',[
+        'title'=>'Basic / $5 each',
+        'price'=>'$5',
+        'btn'=>"Compra 1 credito"
+
+        ])
+         
        @endcomponent
      </div>
      <div class="col-12 col-md-4">
-       @component('components.price',['title'=>'Basic','price'=>'$5'])
-       <li>1 credito</li>
-         <li>Tu tienes el control de tu cuenta</li>
-         <li>2 conecciones simultaneas</li>
-         <li>Mas de 40TB de contenido</li>
-         <li>Actualizaciones diarias</li>
-       @endcomponent
-     </div>
-     <div class="col-12 col-md-4">
-       @component('components.price',['title'=>'Premiun','price'=>'$22'])
-         <li>5 creditos</li>
-         <li>Tu tienes el control de tu cuenta</li>
-         <li>2 conecciones simultaneas</li>
-         <li>Mas de 40TB de contenido</li>
-         <li>Actualizaciones diarias</li>
+       @component('components.price',[
+        'title'=>'Premiun/ $4 each',
+        'price'=>'$20',
+        'btn'=>"Compra 5 creditos"]
+        )
+        
+       
        @endcomponent
        </div>
        <div class="col-12 col-md-4">
-       @component('components.price',['title'=>'Advanced','price'=>'$40'])
-       <li>10 creditos</li>
-       <li>Tu tienes el control de tu cuenta</li>
-       <li>2 conecciones simultaneas</li>
-       <li>Mas de 40TB de contenido</li>
-       <li>Actualizaciones diarias</li>
-     @endcomponent
+       @component('components.price',[
+        'title'=>'Advanced/ $3.5 each',
+        'price'=>'$35',
+        'btn'=>"Compra 10 creditos"
+        ])
+        @endcomponent
      </div>
      <div class="col-12 col-md-4">
-       @component('components.price',['title'=>'Master','price'=>'$70'])
-       <li>20 creditos</li>
-       <li>Tu tienes el control de tu cuenta</li>
-       <li>2 conecciones simultaneas</li>
-       <li>Mas de 40TB de contenido</li>
-       <li>Actualizaciones diarias</li>
+       @component('components.price',['title'=>'Master/ $3 each','price'=>'$60'])
+       
      @endcomponent
      </div>
 
