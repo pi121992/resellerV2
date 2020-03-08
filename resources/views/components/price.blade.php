04ff98464168458fcbc8800e9c04c52d74fb4cbd
Miguel Z <pituntizelaya@gmail.com>
2020-03-08 05:01:19 -0500

    se creo un componente para los precios

--- resources/views/components/price.blade.php
+++ resources/views/components/price.blade.php
@@ -7,7 +7,7 @@
             <h1 class="card-title pricing-card-title">{{ $price }} <small class="text-muted"></small></h1>
             <ul class="list-unstyled font-weight-bold text-muted mt-3 mb-4">
               {{ $slot }}
-             <li class="alerr">Cada credito tiene <label class="text-success">duracion de 1 mes</label></li>
+             <li class="text-success">Cada credito tiene duracion de 1 mes</li>
              <li>2 conecciones simultaneas</li>
              <li>Mas de 40TB de contenido</li>
              <li>Actualizaciones diarias</li>
