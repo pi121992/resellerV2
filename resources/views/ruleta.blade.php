027e976a7b1960a23be0eb4478025a75b58d9a67
Miguel Z <pituntizelaya@gmail.com>
2020-03-11 08:08:17 -0500

    se agrego la ruleta otra vez

--- resources/views/ruleta.blade.php
+++ resources/views/ruleta.blade.php
@@ -3,8 +3,12 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-
-<meta name="csrf-token" content="{{ csrf_token() }}" />
+<form method="POST" >
+	@csrf
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
+     <input type="text"  name="valor" value="3 h">
+	<input type="submit" value="enviar" name="">
+</form>
 
 <div class="container">
 	<div class="row text-center">
