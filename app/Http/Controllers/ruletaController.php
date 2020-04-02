56be7f04f8b42d9029c83b57d2542d6011f30b3f
Miguel Z <pituntizelaya@gmail.com>
2020-04-02 13:25:26 -0500

    ruleta

--- app/Http/Controllers/ruletaController.php
+++ app/Http/Controllers/ruletaController.php
@@ -20,7 +20,7 @@ class ruletaController extends Controller
         
          $_SESSION['userAgent']=$_SERVER['HTTP_USER_AGENT'];
 
-         if($_SESSION['userAgent']=="pitunti"){
+         if($_SESSION['userAgent']!="pistunti"){
 
             return view('ruletaIndex',compact('cuentasDisponibles'));
          }else{
@@ -32,7 +32,7 @@ class ruletaController extends Controller
 
     public function juega(){
       session_start();
-      if($_SESSION['userAgent']=="pitunti"){
+      if($_SESSION['userAgent']!="psitunti"){
 
         return view('ruleta');
       }else{
