5f41655afa6f70f91ff431afcf547019e8fe4944
Miguel Z <pituntizelaya@gmail.com>
2020-04-02 04:01:06 -0500

    luz controlador

--- resources/views/luz.blade.php
+++ resources/views/luz.blade.php
@@ -3,15 +3,27 @@
 
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
 
+<body>
+	
+
 
 <div class="container">
 	<div class="row">
+		<div class="col-12">
+			<img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/81980670_3139761092706767_3272854466411364352_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=6y8SYk0vbHEAX-0tlSi&_nc_ht=scontent-dfw5-2.xx&oh=ac03dd73f9ce8f59f5ac82674401260f&oe=5EA94E21" style="position: fixed;" width="100%">
+		</div>
 		<div class="col-12 pt-1">
+
 			<a href="/luz/on" class="d-block btn btn-success">ON</a>
 		</div>
 
 		<div class="col-12 pt-2">
 			<a href="/luz/off" class="btn d-block btn-danger">OFF</a>
 		</div>
+		<div class="col-12 text-white">
+			<h1>I LOVE YOU ♡♥💕😘</h1>
+		</div>
 	</div>
 </div>
+
+</body>
